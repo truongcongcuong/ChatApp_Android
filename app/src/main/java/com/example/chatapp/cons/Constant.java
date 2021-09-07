37893d6874a3fa4b7d11c8697997a8c7e7d4b26d
maikiencuong <maikiencuongiuh@gmail.com>
2021-09-07 17:56:58 +0700

    [kcuong] Constant: cập nhật endpoint localhost cho máy ảo genymotion

--- app/src/main/java/com/example/chatapp/cons/Constant.java
+++ app/src/main/java/com/example/chatapp/cons/Constant.java
@@ -1,11 +1,11 @@
 package com.example.chatapp.cons;
 
 public class Constant {
-    public static final String API_SIGNUP = "http://10.0.2.2:8080/api/auth/signup/";
-    public static final String API_AUTH = "http://10.0.2.2:8080/api/auth/";
-    public static final String API_INBOX = "http://10.0.2.2:8080/api/inboxs";
-    public static final String API_CHAT = "http://10.0.2.2:8080/api/messages/inbox/";
-    public static final String API_FRIEND_LIST = "http://10.0.2.2:8080/api/friends";
-    public static final String API_CHAT_TO = "http://10.0.2.2:8080/chat";
+    public static final String API_SIGNUP = "http://10.0.3.2:8080/api/auth/signup/";
+    public static final String API_AUTH = "http://10.0.3.2:8080/api/auth/";
+    public static final String API_INBOX = "http://10.0.3.2:8080/api/inboxs";
+    public static final String API_CHAT = "http://10.0.3.2:8080/api/messages/inbox/";
+    public static final String API_FRIEND_LIST = "http://10.0.3.2:8080/api/friends";
+    public static final String API_CHAT_TO = "http://10.0.3.2:8080/chat";
 
 }

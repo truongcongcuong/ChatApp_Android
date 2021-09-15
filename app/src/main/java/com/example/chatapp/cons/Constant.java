7af1005dc3a104bfb61e3d10f44aa36a30d86cca
maikiencuong <maikiencuongiuh@gmail.com>
2021-09-15 13:26:38 +0700

    [kcuong] Constant: thêm api connect websocket

--- app/src/main/java/com/example/chatapp/cons/Constant.java
+++ app/src/main/java/com/example/chatapp/cons/Constant.java
@@ -9,14 +9,16 @@ public class Constant {
     public static final String API_FRIEND_LIST = "http://10.0.3.2:8080/api/friends";
     public static final String API_CHAT_TO = "http://10.0.3.2:8080/chat";
     public static final String API_ME = "http://10.0.3.2:8080/api/user/me";
+    public static final String WEB_SOCKET = "ws://10.0.3.2:8080/ws/websocket";
 
     // Android Studio
-// public static final String API_SIGNUP = "http://10.0.2.2:8080/api/auth/signup/";
+//    public static final String API_SIGNUP = "http://10.0.2.2:8080/api/auth/signup/";
 //    public static final String API_AUTH = "http://10.0.2.2:8080/api/auth/";
 //    public static final String API_INBOX = "http://10.0.2.2:8080/api/inboxs";
 //    public static final String API_CHAT = "http://10.0.2.2:8080/api/messages/inbox/";
 //    public static final String API_FRIEND_LIST = "http://10.0.2.2:8080/api/friends";
 //    public static final String API_CHAT_TO = "http://10.0.2.2:8080/chat";
 //    public static final String API_ME = "http://10.0.2.2:8080/api/user/me";
+//    public static final String WEB_SOCKET= "ws://10.0.2.2:8080/ws/websocket";
 
 }

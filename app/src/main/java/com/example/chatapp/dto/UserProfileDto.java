118df0d468235ba42baf5dfbfeca39bc3ae91478
maikiencuong <maikiencuongiuh@gmail.com>
2021-09-19 15:54:44 +0700

    [kcuong] update UserProfileDto: equal hashcode trên thuộc tính id

--- app/src/main/java/com/example/chatapp/dto/UserProfileDto.java
+++ app/src/main/java/com/example/chatapp/dto/UserProfileDto.java
@@ -1,11 +1,12 @@
 package com.example.chatapp.dto;
 
 import java.io.Serializable;
-import java.util.Date;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 @Data
+@EqualsAndHashCode(of = "id")
 public class UserProfileDto implements Serializable {
     private String id;
     private String displayName;

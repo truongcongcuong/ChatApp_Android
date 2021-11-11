6b85fd627fc9bb15233a4c1ea1aa5cb5d6ecac84
maikiencuong <maikiencuongiuh@gmail.com>
2021-11-11 10:26:01 +0700

    [kcuong] fix some syntax error

--- app/src/main/java/com/example/chatapp/ui/main/frag/InfoFragment.java
+++ app/src/main/java/com/example/chatapp/ui/main/frag/InfoFragment.java
@@ -4,10 +4,8 @@ import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.content.res.Configuration;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
-import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -24,8 +22,6 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.core.widget.NestedScrollView;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
 
 import com.android.volley.DefaultRetryPolicy;
 import com.android.volley.NetworkResponse;
@@ -39,7 +35,7 @@ import com.bumptech.glide.Glide;
 import com.example.chatapp.R;
 import com.example.chatapp.adapter.MenuButtonAdapterVertical;
 import com.example.chatapp.cons.Constant;
-import com.example.chatapp.cons.SendData;
+import com.example.chatapp.cons.SendingData;
 import com.example.chatapp.dto.MenuItem;
 import com.example.chatapp.dto.UserSummaryDTO;
 import com.example.chatapp.entity.Language;
@@ -160,7 +156,7 @@ public class InfoFragment extends Fragment {
                 signout();
             } else if (item.getKey().equals("infoApp")) {
                 Toast.makeText(getActivity(), getString(R.string.app_info), Toast.LENGTH_SHORT).show();
-            } else if(item.getKey().equals("changeLanguage")){
+            } else if (item.getKey().equals("changeLanguage")) {
                 showDialogChangeLanguage();
             }
         });
@@ -172,7 +168,7 @@ public class InfoFragment extends Fragment {
     }
 
     private void showDialogChangeLanguage() {
-        TextView txt_change_language_english,txt_change_language_vietnamese;
+        TextView txt_change_language_english, txt_change_language_vietnamese;
         Dialog dialog = new Dialog(getContext());
         dialog.setContentView(R.layout.dialog_change_language);
         dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
@@ -188,19 +184,19 @@ public class InfoFragment extends Fragment {
                 getString(R.string.language_english_code));
 
         SharedPreferences sharedPreferencesLanguage = getContext().getSharedPreferences("multi-language", Context.MODE_PRIVATE);
-        Language currentLanguage = gson.fromJson(sharedPreferencesLanguage.getString("language",null),Language.class);
+        Language currentLanguage = gson.fromJson(sharedPreferencesLanguage.getString("language", null), Language.class);
         if (currentLanguage.getCode().equals("en"))
-            txt_change_language_english.setCompoundDrawablesWithIntrinsicBounds(R.drawable.united_kingdom,0,R.drawable.ic_baseline_check_circle_outline_24,0);
+            txt_change_language_english.setCompoundDrawablesWithIntrinsicBounds(R.drawable.united_kingdom, 0, R.drawable.ic_baseline_check_circle_outline_24, 0);
         else
-            txt_change_language_vietnamese.setCompoundDrawablesWithIntrinsicBounds(R.drawable.vietnam,0,R.drawable.ic_baseline_check_circle_outline_24,0);
+            txt_change_language_vietnamese.setCompoundDrawablesWithIntrinsicBounds(R.drawable.vietnam, 0, R.drawable.ic_baseline_check_circle_outline_24, 0);
 
-        txt_change_language_english.setOnClickListener(v->{
+        txt_change_language_english.setOnClickListener(v -> {
             languageUtils.changeLanguage(languageEN);
             reloadActivity();
             dialog.dismiss();
         });
 
-        txt_change_language_vietnamese.setOnClickListener(v->{
+        txt_change_language_vietnamese.setOnClickListener(v -> {
             languageUtils.changeLanguage(languageVI);
             reloadActivity();
             dialog.dismiss();
@@ -210,9 +206,8 @@ public class InfoFragment extends Fragment {
     }
 
     private void reloadActivity() {
-
-        SendData sendData = (SendData) getContext();
-        sendData.SendingData("true");
+        SendingData sendData = (SendingData) getContext();
+        sendData.sendString("true");
     }
 
     // set dynamic height for list view
